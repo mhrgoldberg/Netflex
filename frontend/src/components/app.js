c0c72d368bb72e5b75d2ddbf15ec3b2f6689204e
mitchellreiss <mitchellreiss@gmail.com>
2019-11-27 10:25:22 -0800

    Update h1 link in navbar

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch, Route } from "react-router-dom";
-// import { Route }
 import NavBarContainer from "./nav/navbar_container"
 import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
