64f1e52c96ccf67171fb28d535f56feb0a70628b
js4484 <js4484@columbia.edu>
2019-12-03 17:35:48 -0500

    Prepare for heroku deployment

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 const users = require("./routes/api/users");
 const movies = require("./routes/api/movies");
 const search = require('./routes/api/search');
