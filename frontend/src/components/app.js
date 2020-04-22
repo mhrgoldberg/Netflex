73c8e1185ad02ae69bfb84ee9a5117aae863c32d
mitchellreiss <mitchellreiss@gmail.com>
2020-04-21 23:19:40 -0700

    Remove unecessary import

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container"
 import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
